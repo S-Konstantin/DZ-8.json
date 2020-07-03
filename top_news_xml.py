c47e1a4f205e212725a3cdd2c663aca182db6b28
S-Konstantin <s.kostya163@gmail.com>
2020-07-03 20:12:11 +0400

    added files top_news_xml end newsafr.xml

---
+++ top_news_xml.py
@@ -0,0 +1,60 @@
+def read_files():
+    '''
+    Функция чтения файлов
+    '''
+
+    import xml.etree.ElementTree as ET
+
+    parser = ET.XMLParser(encoding="utf-8")
+    tree = ET.parse("/Users/konstantinsealov/Desktop/Python/HomeWork-DZ-8/py-homework-basic-files/newsafr.xml", parser)
+    root = tree.getroot()
+    data = root.findall("channel/item/description")
+    original_text = ''
+    for items in data:
+        original_text += items.text
+    return original_text.lower()
+
+def count_word(original_text):
+    '''
+    функция подсчета слов длиннее 6 символов
+    '''
+    to_list = original_text.split(' ')
+    word_value = {}
+
+    for word in to_list:
+        if len(word) > 6:
+            if word in word_value:
+                word_value[word] += 1
+            else:
+                word_value[word] = 1
+    return word_value
+
+def sort_top(word_value):
+    '''
+    функция сортировки и вывода ТОП-10
+    '''
+
+    l = lambda word_value: word_value[1]
+    sort_list = sorted(word_value.items(), key=l, reverse=True)
+    count = 1
+    top_10 = {}
+
+    for word in sort_list:
+        top_10[count] = word
+        count += 1
+        if count == 11:
+            break
+    return top_10
+
+def main():
+    '''
+    главная функция:  запускает другие функции
+    '''
+
+    print('Открытие файла ...')
+    top_10 = sort_top(count_word(read_files()))
+
+    for i in top_10.values():
+        print(i[1], ' : ', i[0])
+
+main()
